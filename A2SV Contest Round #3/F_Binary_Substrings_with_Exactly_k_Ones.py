09372f6c648a89cfc7fa30e4716b2de064cfae10
Segni Girma <segnigirma11@gmail.com>
2025-02-17 10:29:56 +0300

    F

--- A2SV Contest Round #3/F_Binary_Substrings_with_Exactly_k_Ones.py
+++ A2SV Contest Round #3/F_Binary_Substrings_with_Exactly_k_Ones.py
@@ -1,7 +1,8 @@
 k = int(input())
 from collections import Counter
 nums = []
-for n in input():
+string = input()
+for n in string:
     nums.append(int(n))
     
 pref = [0]
@@ -9,26 +10,14 @@ for n in nums:
     pref.append(pref[-1] + n)
     
 counter = Counter(pref)
-count = 1
-nums = list(set(pref))
-nums.sort()
 res = 0
-
-left = 0
-right = 0
 if k == 0:
-    for i in range(counter[0]):
-        res += i
+    list_str = string.split("1")
+    for s in list_str:
+        res += (len(s)) * (len(s) + 1)// 2
+    
     print(res)
 else:
-    while right< len(nums):
-        if nums[right]  - nums[left] == k:
-            res += counter[nums[left]] * counter[nums[right]]
-            right += 1
-            left += 1
-        elif nums[right]  - nums[left] > k:
-            left += 1
-        else:
-            right += 1
-            
+    for n in counter:
+        res += counter[n] * counter.get(n -k, 0)
     print(res)
